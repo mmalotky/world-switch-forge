66efb846c2f5a90d91d12d70f7d39028f17be2c9
Martin Malotky <mmalotky@gmail.com>
2023-06-22 12:15:44 -0500

    access server.properties

--- src/main/java/com/mmalotky/worldswitch/commands/SetWorldCommand.java
+++ src/main/java/com/mmalotky/worldswitch/commands/SetWorldCommand.java
@@ -10,6 +10,13 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.entity.Entity;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class SetWorldCommand {
     public SetWorldCommand(CommandDispatcher<CommandSourceStack> dispatcher) {
         dispatcher.register(Commands.literal("world")
@@ -18,9 +25,21 @@ public class SetWorldCommand {
 
     private int setWorld(CommandSourceStack source)  {
         Entity entity = source.getEntity();
-        String files = source.getServer().getFile(".").list().toString();
+        File serverProperties = source.getServer().getFile("./server.properties");
+        String print = "err";
 
-        Component comp = new TranslatableComponent("chat.type.announcement", source.getDisplayName(), files);
+        try (Scanner reader = new Scanner(serverProperties)) {
+            while (reader.hasNextLine()) {
+                String data = reader.nextLine();
+                if(data.contains("level-name=")) {
+                    print = data;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        Component comp = new TranslatableComponent("chat.type.announcement", source.getDisplayName(), print);
         source.getServer().getPlayerList().broadcastMessage(comp, ChatType.CHAT, entity != null ? entity.getUUID() : Util.NIL_UUID);
 
         return Command.SINGLE_SUCCESS;
