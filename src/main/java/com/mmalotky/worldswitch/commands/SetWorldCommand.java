c8b1793b2b4a2651c495d8db044b480c78e9921d
Martin Malotky <mmalotky@gmail.com>
2023-06-22 10:21:52 -0500

    attempt to access file

--- src/main/java/com/mmalotky/worldswitch/commands/SetWorldCommand.java
+++ src/main/java/com/mmalotky/worldswitch/commands/SetWorldCommand.java
@@ -8,6 +8,10 @@ import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.entity.player.Player;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class SetWorldCommand {
     public SetWorldCommand(CommandDispatcher<CommandSourceStack> dispatcher) {
         dispatcher.register(Commands.literal("world")
@@ -15,7 +19,22 @@ public class SetWorldCommand {
     }
 
     private int setWorld(CommandSourceStack source)  {
-        if(source.getEntity() instanceof Player) source.getEntity().sendMessage(new TextComponent("test"), Util.NIL_UUID);
+
+        try {
+            File serverProperties = source.getServer().getFile("server.properties");
+            Scanner myReader = new Scanner(serverProperties);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                if(data.contains("level-name=")) {
+                    if(source.getEntity() instanceof Player) source.getEntity().sendMessage(new TextComponent(data), Util.NIL_UUID);
+                }
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
         return Command.SINGLE_SUCCESS;
     }
 }
