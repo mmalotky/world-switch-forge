8338d79642b55d88b700f4fec8cacb5a3a22d91e
Martin Malotky <mmalotky@gmail.com>
2023-06-23 17:26:43 -0500

    save player data

--- src/main/java/com/mmalotky/worldswitch/commands/SetWorldCommand.java
+++ src/main/java/com/mmalotky/worldswitch/commands/SetWorldCommand.java
@@ -8,6 +8,11 @@ import net.minecraft.commands.Commands;
 import org.slf4j.Logger;
 
 import java.io.*;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
 
 public class SetWorldCommand {
     private static final Logger LOGGER = LogUtils.getLogger();
@@ -19,6 +24,9 @@ public class SetWorldCommand {
     private int setWorld(CommandSourceStack source) {
         String world = source.getServer().getWorldData().getLevelName();
         File worldFile = source.getServer().getFile(String.format("./%s", world));
+
+        savePlayerData(worldFile);
+
         if (!deleteDirectory(worldFile)) {
             LOGGER.error(String.format("Error: Unable to delete world file %s.", world));
             return 0;
@@ -26,6 +34,29 @@ public class SetWorldCommand {
         source.getServer().halt(false);
 
         return Command.SINGLE_SUCCESS;
+    }
+
+    private void savePlayerData(File worldFile) {
+        //select files
+        File[] files = worldFile.getParentFile().listFiles();
+        if(files == null) return;
+        File saveFile = Arrays.stream(files)
+                .filter(a -> a.getName().equals("playerData"))
+                .findFirst()
+                .orElse(new File(worldFile.getParentFile().getAbsolutePath() + "/playerData"));
+        if(!saveFile.exists()) saveFile.mkdir();
+
+        //copy data
+        LOGGER.info("Caching Player Data.");
+        try {
+            Path playerData = Path.of(worldFile.getAbsolutePath() + "/playerdata");
+            Path playerDataCopy = Path.of(saveFile.getAbsolutePath() + "/playerdata");
+            Files.copy(playerData, playerDataCopy, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage());
+            e.printStackTrace();
+        }
+
     }
 
     private boolean deleteDirectory(File directory) {
