94e7193ed21a14d12f33b1fb4a0305ba5d30ee49
Martin Malotky <mmalotky@gmail.com>
2023-06-24 08:33:29 -0500

    update position and spawn

--- src/main/java/com/mmalotky/worldswitch/events/ModEvents.java
+++ src/main/java/com/mmalotky/worldswitch/events/ModEvents.java
@@ -4,6 +4,10 @@ import com.mmalotky.worldswitch.IO.IOMethods;
 import com.mmalotky.worldswitch.WorldSwitch;
 import com.mmalotky.worldswitch.commands.SetWorldCommand;
 import com.mojang.logging.LogUtils;
+import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -12,6 +16,7 @@ import org.slf4j.Logger;
 
 import java.io.File;
 import java.nio.file.Path;
+import java.util.List;
 
 @Mod.EventBusSubscriber(modid = WorldSwitch.MOD_ID)
 public class ModEvents {
@@ -34,5 +39,19 @@ public class ModEvents {
 
         LOGGER.info("Injecting player data.");
         IOMethods.copyDirectory(playerData, playerDataCopy);
+
+        LOGGER.info("Updating spawn");
+        int x = event.getServer().getWorldData().overworldData().getXSpawn();
+        int y = event.getServer().getWorldData().overworldData().getYSpawn();
+        int z = event.getServer().getWorldData().overworldData().getZSpawn();
+        float angle = event.getServer().getWorldData().overworldData().getSpawnAngle();
+
+        ResourceKey<Level> resourceKey = event.getServer().overworld().dimension();
+        List<ServerPlayer> playerList = event.getServer().getPlayerList().getPlayers();
+        for (ServerPlayer player : playerList) {
+            player.setPos(x,y,z);
+            player.setRespawnPosition(resourceKey, new BlockPos(x,y,z), angle, true,false);
+        }
+
     }
 }
